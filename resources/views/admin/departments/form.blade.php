fc628700534de081add2a54a66387462de88f207
Dao Ai Giang Nam <31945782+daoaigiangnam@users.noreply.github.com>
2026-09-01 19:55:42 +0700

    feat: remove department active field

--- resources/views/admin/departments/form.blade.php
+++ resources/views/admin/departments/form.blade.php
@@ -4,10 +4,10 @@
 <div class="card form">
 <form method="post" action="{{ $department->exists ? route('admin.departments.update',$department) : route('admin.departments.store') }}">
  @csrf @if($department->exists) @method('PUT') @endif
- <div class="field"><label>Code *</label><input class="input" name="code" value="{{ old('code',$department->code) }}" required></div>
- <div class="field"><label>Department Name *</label><input class="input" name="name" value="{{ old('name',$department->name) }}" required></div>
- <div class="field"><label>Description</label><input class="input" name="description" value="{{ old('description',$department->description) }}"></div>
- <div class="field"><label><input type="checkbox" name="is_active" value="1" @checked(old('is_active',$department->is_active))> Active</label></div>
+ <h3 style="margin-top:0">Department Information</h3>
+ <div class="field"><label>Code *</label><input class="input" name="code" value="{{ old('code',$department->code) }}" required placeholder="DEPT-001"></div>
+ <div class="field"><label>Department Name *</label><input class="input" name="name" value="{{ old('name',$department->name) }}" required placeholder="IT Department"></div>
+ <div class="field"><label>Description</label><input class="input" name="description" value="{{ old('description',$department->description) }}" maxlength="255"></div>
  <button class="btn" type="submit">Save Department</button> <a class="btn gray" href="{{ route('admin.departments.index') }}">Cancel</a>
 </form></div>
 @endsection
